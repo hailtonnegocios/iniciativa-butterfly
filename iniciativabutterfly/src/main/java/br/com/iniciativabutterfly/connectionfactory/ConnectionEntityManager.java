bf5272eae223998fe78323af832f24d0fddaa4b1
hailtonnegocios@gmail.com <hailtonnegocios@gmail.com@93cb72ae-8021-0200-1015-a51d89be19c1>
2014-12-02 02:23:00 +0000

    Iniciativa-Butterfly Project no SVN

---
+++ iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/connectionfactory/ConnectionEntityManager.java
@@ -0,0 +1,25 @@
+package br.com.iniciativabutterfly.connectionfactory;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class ConnectionEntityManager {
+	
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("IniciativaButterfly");
+
+	@Produces @RequestScoped
+	public EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public void close(@Disposes EntityManager manager){
+		manager.close();
+	}
+
+
+}
---
+++ iniciativabutterfly/target/iniciativabutterfly-0.0.1-SNAPSHOT/WEB-INF/jsp/testeVraptor/adiciona.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<div id="mensagem">
+    <h1>Framework : ${tv.nomeFramework}</h1>
+    <h2>Tecnologias : ${tv.descricaoFramework}</h2>
+    <br>
+    Versão : ${tv.version}
+</div>
+
+</body>
+</html>

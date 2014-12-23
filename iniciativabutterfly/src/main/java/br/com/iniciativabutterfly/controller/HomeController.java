bb216deee9d11dfaa5595f0a8736a0372f86be40
rasa.larig@gmail.com <rasa.larig@gmail.com@93cb72ae-8021-0200-1015-a51d89be19c1>
2014-12-23 05:38:38 +0000

    Criação Inicial da identidade visual, home do site, e ajuste de mapeamento de generatedValue com Identity

---
+++ iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.iniciativabutterfly.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class HomeController {
+
+	@Path("/")
+	public void index(){
+		
+	}
+}

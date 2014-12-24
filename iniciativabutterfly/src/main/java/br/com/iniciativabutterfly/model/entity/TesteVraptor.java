45b91d6e11980db2dd3989098ac9b3c31e706a54
hailtonnegocios@gmail.com <hailtonnegocios@gmail.com@93cb72ae-8021-0200-1015-a51d89be19c1>
2014-12-24 02:50:19 +0000

    Alteração na Entity

--- iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/model/entity/TesteVraptor.java
+++ iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/model/entity/TesteVraptor.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,18 +12,22 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@EqualsAndHashCode @ToString
+@EqualsAndHashCode
+@ToString
 public class TesteVraptor {
-	
+
 	@Id  
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter	@Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
 	private String nomeFramework;
-	
-	@Getter	@Setter
+
+	@Getter
+	@Setter
 	private String descricaoFramework;
 
-	@Getter	@Setter
+	@Getter
+	@Setter
 	private String version;
-	
+
 }

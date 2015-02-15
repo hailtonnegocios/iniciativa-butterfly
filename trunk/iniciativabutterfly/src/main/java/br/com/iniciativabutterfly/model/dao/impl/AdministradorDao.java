8b5427a9cc4ebc066c1b7d84c55ee1f7edd3612b
viniciusribeirosp@gmail.com <viniciusribeirosp@gmail.com@93cb72ae-8021-0200-1015-a51d89be19c1>
2015-02-15 20:07:49 +0000

    Atualiza e Remove (em teste)

--- trunk/iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/model/dao/impl/AdministradorDao.java
+++ trunk/iniciativabutterfly/src/main/java/br/com/iniciativabutterfly/model/dao/impl/AdministradorDao.java
@@ -12,7 +12,7 @@ import javax.persistence.EntityManager;
 import br.com.iniciativabutterfly.model.entity.AdministradorModel;
 
 public class AdministradorDao {
-	
+
 	private final EntityManager em;
 	
 	@Inject
@@ -29,8 +29,8 @@ public class AdministradorDao {
  * @return Retorna se este usuario administrador ja existe
  */
 	public boolean existe(AdministradorModel administradorModel) {
-		return !em.createQuery("select u from administradormodel u where u.email = "
-			+ ":email and u.senha = :senha", AdministradorModel.class)
+		return !em.createQuery("from administradormodel u where u.email = "
+				+ ":email and u.senha = :senha", AdministradorModel.class)
 			.setParameter("email", administradorModel.getEmail())
 			.setParameter("senha", administradorModel.getSenha())
 			.getResultList().isEmpty();
@@ -41,26 +41,34 @@ public class AdministradorDao {
 			//Se o usuario nao existir eu adiciono ele no BD
 			em.getTransaction().begin();
 			em.persist(administradorModel);
-			em.getTransaction().commit();	
+			em.getTransaction().commit();
+			em.close();
 //		}
 		System.out.println("Usuario ja cadastrado no sistema");
 	}
 	
 	public void altera(AdministradorModel administradorModel) {
+		em.getTransaction().begin();
 		em.merge(administradorModel);
+		em.getTransaction().commit();
+		em.close();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<AdministradorModel> lista() {
 		List<AdministradorModel> lista = em.createQuery("from AdministradorModel as model order by model.id").getResultList();
+		em.close();
 		return lista;
 	}
 	
-	public AdministradorModel buscaPorID(AdministradorModel administradorModel) {
-		return em.find(AdministradorModel.class, administradorModel.getId());
+	public AdministradorModel buscaPorID(Long id) {
+		return em.find(AdministradorModel.class, id);
 	}
 	
-	public void remove(AdministradorModel administradorModel) {
-		em.remove(buscaPorID(administradorModel));
+	public void remove(Long id) {
+		em.getTransaction().begin();
+		em.remove(id);
+		em.getTransaction().commit();
+		em.close();
 	}
 }
